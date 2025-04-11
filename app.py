c91de5cbdae51007bd44741a970de6a4abaf41b7
alexlines8 <alexlines8@yahoo.co.uk>
2025-04-11 21:09:00 +0100

    render database

--- app.py
+++ app.py
@@ -55,6 +55,12 @@ def logout():
     flash('You have been logged out.', 'info')
     return redirect(url_for('home'))
 
+
+@app.route('/create-db')
+def create_db():
+    db.create_all()
+    return 'Database created!'
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
