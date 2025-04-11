48b16954e5900f87d34dde400cfd46155cfec502
alexlines8 <alexlines8@yahoo.co.uk>
2025-04-11 20:41:23 +0100

    Fixed setup

--- app.py
+++ app.py
@@ -1,21 +1,10 @@
 from flask import Flask, render_template
-from models import db
-from flask_login import LoginManager
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'your-secret-key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-
-db.init_app(app)
-
-login_manager = LoginManager(app)
-
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
+if __name__ == "__main__":
     app.run(debug=True)
